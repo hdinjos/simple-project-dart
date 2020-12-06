1035dd618a4e38ca75b4730a9624c3be29ea92ef
hdinjos <hdinjos@gmail.com>
2020-12-06 13:37:32 +0700

    menambah komentar

--- bin/app_baik.dart
+++ bin/app_baik.dart
@@ -1,6 +1,6 @@
 import 'package:app_baik/display.dart';
-import 'package:random_string/random_string.dart';
-import 'package:app_baik/display.dart';
+import 'package:random_string/random_string.dart'; //import package from external (pub)
+import 'package:app_baik/display.dart'; //import package from internal in forlder lib (custom package)
 
 void main(List<String> arguments) {
   print(randomBetween(10, 20));
