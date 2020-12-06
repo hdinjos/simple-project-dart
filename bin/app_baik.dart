ee30c0fbb44bb32a452d0b177265fb8fd35e6830
hdinjos <hdinjos@gmail.com>
2020-12-06 13:32:47 +0700

    import custom package from lib folder

--- bin/app_baik.dart
+++ bin/app_baik.dart
@@ -1,6 +1,9 @@
+import 'package:app_baik/display.dart';
 import 'package:random_string/random_string.dart';
+import 'package:app_baik/display.dart';
 
 void main(List<String> arguments) {
   print(randomBetween(10, 20));
   print('Hello world!');
+  display();
 }
