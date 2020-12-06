e69661e3546423645636fb08cd0bceb3e51f48f6
hdinjos <hdinjos@gmail.com>
2020-12-06 13:44:51 +0700

    manambah keterangan unuk folder bin

--- bin/app_baik.dart
+++ bin/app_baik.dart
@@ -3,7 +3,7 @@ import 'package:random_string/random_string.dart'; //import package from externa
 import 'package:app_baik/display.dart'; //import package from internal in forlder lib (custom package)
 
 void main(List<String> arguments) {
-  print(randomBetween(10, 20));
-  print('Hello world!');
-  display();
+  print(randomBetween(10, 20)); // function from package random_string
+  print('Hello world!'); // code in file
+  display(); //function from /lib/display.dart
 }
